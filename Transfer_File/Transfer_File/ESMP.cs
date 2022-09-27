864b840bae1564d690947b9f674aa693527b7847
deryckgod <410721203@gms.ndhu.edu.tw>
2022-09-27 15:26:41 +0800

    V6剩改DATE

--- Transfer_File/Transfer_File/ESMP.cs
+++ Transfer_File/Transfer_File/ESMP.cs
@@ -2,6 +2,7 @@
 using System.Configuration;
 using System.Data;
 
+
 namespace Transfer_File
 {
     class T30
@@ -88,31 +89,7 @@ namespace Transfer_File
         int cfm24 = 0;
         string cfm25 = "";
 
-        public string Cfm01 { get => cfm01; set => cfm01 = value; }
-        public string Cfm02 { get => cfm02; set => cfm02 = value; }
-        public char Cfm03 { get => cfm03; set => cfm03 = value; }
-        public string Cfm04 { get => cfm04; set => cfm04 = value; }
-        public decimal Cfm05 { get => cfm05; set => cfm05 = value; }
-        public decimal Cfm06 { get => cfm06; set => cfm06 = value; }
-        public decimal Cfm07 { get => cfm07; set => cfm07 = value; }
-        public char Cfm08 { get => cfm08; set => cfm08 = value; }
-        public decimal Cfm09 { get => cfm09; set => cfm09 = value; }
-        public decimal Cfm10 { get => cfm10; set => cfm10 = value; }
-        public decimal Cfm11 { get => cfm11; set => cfm11 = value; }
-        public string Cfm12 { get => cfm12; set => cfm12 = value; }
-        public int Cfm13 { get => cfm13; set => cfm13 = value; }
-        public int Cfm14 { get => cfm14; set => cfm14 = value; }
-        public int Cfm15 { get => cfm15; set => cfm15 = value; }
-        public int Cfm16 { get => cfm16; set => cfm16 = value; }
-        public int Cfm17 { get => cfm17; set => cfm17 = value; }
-        public int Cfm18 { get => cfm18; set => cfm18 = value; }
-        public int Cfm19 { get => cfm19; set => cfm19 = value; }
-        public int Cfm20 { get => cfm20; set => cfm20 = value; }
-        public int Cfm21 { get => cfm21; set => cfm21 = value; }
-        public int Cfm22 { get => cfm22; set => cfm22 = value; }
-        public string Cfm23 { get => cfm23; set => cfm23 = value; }
-        public int Cfm24 { get => cfm24; set => cfm24 = value; }
-        public string Cfm25 { get => cfm25; set => cfm25 = value; }
+        
 
 
         #endregion
